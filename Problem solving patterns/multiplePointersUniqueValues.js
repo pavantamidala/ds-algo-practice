a01494a7fe107b8f9381f950d8b3c7874e19bf81
pavantamidala <pavantamidala023@gmail.com>
2022-04-06 06:54:47 +0530

    added multiple pointers unique values

---
+++ Problem solving patterns/multiplePointersUniqueValues.js
@@ -0,0 +1,14 @@
+function uniqueValues(arr) {
+  let i = 0;
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i = i + 1;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+let arr = [1, 1, 1, 2, 3, 3, 4, 4, 5, 5, 6, 6, 6];
+
+let res = uniqueValues(arr);
+console.log(res);
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // import './Problem solving patterns/frequencyCounter.js';
 // import './Problem solving patterns/frequencyAnagram.js';
-import './Problem solving patterns/multiplePointers.js';
+// import './Problem solving patterns/multiplePointers.js';
+import './Problem solving patterns/multiplePointersUniqueValues.js';
 // multiplePointers
+// multiplePointersUniqueValues
