baddbc33fd763da6efbe351bea796d5822aa21de
pavantamidala <pavantamidala023@gmail.com>
2022-05-13 07:20:24 +0530

    added pop for linked list

--- data structures/linkedList.js
+++ data structures/linkedList.js
@@ -1,35 +1,54 @@
-class Node{
-  constructor(val){
-    this.val = val
-    this.next = null
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
 }
 
-class LinkedList{
-  constructor(){
-    this.length=0
-    this.head = null 
-    this.tail = null
+class LinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
   }
-  push(val){
-    if(!this.length){
-      this.head = new Node(val)
-      this.tail = this.head
-      this.length++
-    }else{
-      this.tail.next = new Node(val)
-      this.tail = this.tail.next
-      this.length++
+  push(val) {
+    if (!this.length) {
+      this.head = new Node(val);
+      this.tail = this.head;
+      this.length++;
+    } else {
+      this.tail.next = new Node(val);
+      this.tail = this.tail.next;
+      this.length++;
     }
   }
+
+  pop() {
+    if (!this.length) {
+      return undefined;
+    }
+    let current = this.head;
+    let newTail = this.head;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    console.log(current.val);
+    console.log(newTail.val);
+    this.tail = newTail;
+    this.length--;
+    return current;
+  }
 }
 
-let list = new LinkedList()
-list.push('hi')
-list.push('how')
-console.log(list)
-
-
+let list = new LinkedList();
+list.push('hi');
+list.push('how');
+list.push('are');
+// console.log(list)
+let val = list.pop();
+console.log(val);
+console.log(list.length);
 
 //original solution
 
