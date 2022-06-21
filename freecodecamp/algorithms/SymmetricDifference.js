16a71fdb839da6bff4c159e1f41d10cbe1ae6c5f
pavantamidala <pavantamidala023@gmail.com>
2022-06-21 08:49:30 +0530

    ftr:added symmetric difference problem

--- freecodecamp/algorithms/SymmetricDifference.js
+++ freecodecamp/algorithms/SymmetricDifference.js
@@ -1,6 +1,18 @@
+import createCharMap from "../../utils/CharMap.js";
+
 function symmetricDifference(arr1, arr2) {
-  return;
+  let charMap1 = createCharMap(arr1);
+  let charMap2 = createCharMap(arr2);
+  let ans1 = arr1.filter((val) => {
+    return !charMap2[val];
+  });
+  let ans2 = arr2.filter((val) => {
+    return !charMap1[val];
+  });
+  return ans1.concat(ans2);
+  // console.log(ans1);
+  // console.log(ans2);
 }
 
-let result = symmetricDifference([1, 2, 3], [5, 2, 1, 4]);
+let result = symmetricDifference([1, 2, 3], [5, 2, 1, 1, 4]);
 console.log(result);
