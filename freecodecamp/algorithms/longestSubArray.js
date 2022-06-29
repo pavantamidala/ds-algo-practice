d5cd90371eeeec7a12ed322c86442ae4f57309cd
pavantamidala <pavantamidala023@gmail.com>
2022-06-29 20:18:59 +0530

    added longest subarray

---
+++ freecodecamp/algorithms/longestSubArray.js
@@ -0,0 +1,8 @@
+function largestOfFour(arr) {
+    let ans  =     arr.map(function(v) { 
+        return Math.max(...v)
+      });
+      console.log(ans);
+  }
+  
+  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
