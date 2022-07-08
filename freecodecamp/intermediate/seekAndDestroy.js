fab7accec5212e207673ca31ca51cf1a7a5e439e
pavantamidala <pavantamidala023@gmail.com>
2022-07-08 23:04:33 +0530

    added seek and destroy problem

--- freecodecamp/intermediate/seekAndDestroy.js
+++ freecodecamp/intermediate/seekAndDestroy.js
@@ -1,6 +1,14 @@
+import createCharMap from '../../utils/CharMap.js'
+
 function destroyer(arr) {
-    // return arr;
-    console.log(Object.keys(arguments));
+    let mainArr = arguments[0]
+    let itemsArr = Object.values(arguments).filter((v,i)=>i!=0)
+    let map1 = createCharMap(itemsArr);
+
+    mainArr =  mainArr.filter((item)=>{
+      return !map1[item]
+    })
+
   }
-  
+
   destroyer([1, 2, 3, 1, 2, 3], 2, 3);
