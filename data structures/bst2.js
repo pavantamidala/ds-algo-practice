2fcef18fc38577c5950f2a40f141dd0192998923
pavantamidala <pavantamidala023@gmail.com>
2022-10-31 18:47:39 +0530

    bst tree

--- data structures/bst2.js
+++ data structures/bst2.js
@@ -21,22 +21,50 @@ class BinarySearchTree {
         }
         let current = this.root
         while (true) {
+            if (value === current.value) return undefined;
             if (current.value < value) {
                 if (!current.left) {
                     current.left = node;
                     return this;
                 }
-                else {
-                    current = current.left
-                }
-            } else if (current.value > value) {
+                current = current.left
+            }
+            else {
                 if (!current.right) {
                     current.right = node;
                     return this;
-                } else {
-                    current = current.right
                 }
+                current = current.right
             }
         }
     }
+
+    find(value) {
+        if (this.root === null) return false;
+
+        let current = this.root
+        let found = false;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
+
+
+let tree = new BinarySearchTree()
+
+tree.insert(10)
+tree.insert(100)
+tree.insert(20)
+tree.insert(30)
+tree.insert(40)
+
+tree.contains(10)
