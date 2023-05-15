7ef6c3e45e5799d17e1cc17bba4a8f161b2eb88b
pavantamidala <62680117+pavantamidala@users.noreply.github.com>
2023-05-15 19:28:43 +0530

    1 file added

---
+++ project-euler/Smallest multiple.js
@@ -0,0 +1,12 @@
+function smallestMultiple(limit, length) {
+  let ans = 0;
+  for (let i = 1; i <= length; i++) {
+    for (let j = 1; j <= limit; j++) {
+      if (j % i === 0) {
+        ans = j;
+        break;
+      }
+    }
+  }
+  return ans;
+}
