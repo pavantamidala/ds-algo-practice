fd9d197fd8f85c5899cb54ea7b7111307dd3ea9d
pavantamidala <62680117+pavantamidala@users.noreply.github.com>
2023-06-07 00:15:19 +0530

    1 file modified

--- project-euler/Smallest multiple.js
+++ project-euler/Smallest multiple.js
@@ -2,9 +2,27 @@ import logObject from '../utils/logObject.js';
 
 function smallestMultiple() {
   let ans = 0;
-  for (let i = 1; i <= 20; i++) {
-    for (let j = 1; j < Infinity; j++) {}
+  let i = 1;
+  for (let j = 1; j < Infinity; j++) {
+    for (i; i <= 20; i++) {
+      if (i % j === 0) {
+        return;
+      }
+    }
+    if (j === 20) {
+      console.log(j);
+      return j;
   }
   return ans;
 }
 logObject(smallestMultiple());
+// function temp() {
+//   for (let i = 0; i < 10; i++) {
+//     console.log(i);
+//     for (let j = 1; j < 5; j++) {
+//       console.log(j, 'j');
+//     }
+//     return;
+//   }
+// }
+// temp();
